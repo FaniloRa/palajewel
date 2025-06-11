c3bfe3ccecdd6994ffb1c0b0b1faa57fc708bae2
Fanilomahery Elder <fanilomaheryrandriama@gmail.com>
2025-06-11 03:54:35 +0000

    Fait monter un peut le contenue à gauche du Hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <Header />
       <div className="flex-grow flex container mx-auto px-4 sm:px-6 lg:px-4 items-center pt-24 md:pt-32"> {/* Added padding-top for header */}
         {/* Left Content Area */}
-        <div className="w-full lg:w-1/2 xl:w-2/5 flex flex-col justify-center text-left py-10 z-10">
+        <div className="w-full lg:w-1/2 xl:w-2/5 flex flex-col justify-center text-left pt-8 pb-10 z-10">
           <span className="font-snippet font-normal text-sm sm:text-base uppercase tracking-widest text-accent/80 mb-2 sm:mb-3">
             EDITION LIMITEE
           </span>
