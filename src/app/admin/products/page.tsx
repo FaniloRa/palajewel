eaff6c9a01ec445f381fa8ecbcf99ff57c820655
Fanilomahery Elder <fanilomaheryrandriama@gmail.com>
2025-07-02 12:50:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -7,7 +7,7 @@ import ProductsPageClient from './products-client-page'
 
 export default async function ProductsPage() {
   await connectDB();
-  const products: OurProduct[] = JSON.parse(JSON.stringify(await Product.find({}).sort({ createdAt: -1 })));
+  const products: OurProduct[] = JSON.parse(JSON.stringify(await Product.find({}).populate('category').sort({ createdAt: -1 })));
 
   return <ProductsPageClient products={products} />;
 }
